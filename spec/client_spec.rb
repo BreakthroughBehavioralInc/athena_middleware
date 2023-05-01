a8befef379157b6123b4fdac9aa7f0d7162917b9
Robert Magomero <rmagomero@mdlive.com>
2023-05-01 15:20:51 -0400

    update client and patients

---
+++ spec/client_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+RSpec.describe AthenaApi::Client, vcr: { re_record_interval: 604800 } do
+  let(:config) { AthenaApi::Config.new }
+
+  it 'should be able to be initialized with a Config object' do
+    client = AthenaApi::Client.new(config)
+    expect(client.config).to eq(config)
+  end
+
+  describe '#active_client' do
+    it 'should config new client with standard config' do
+      expect(AthenaApi::Client.active_client.config).to eq(AthenaApi.config)
+    end
+  end
+
+  describe "#connection" do
+    it 'should initialize an OAuth2::Client object' do
+      expect(AthenaApi::Client.active_client.connection.is_a?(OAuth2::Client))
+    end
+  end
+
+  describe "#token_connection" do
+    let (:token_connection) { AthenaApi::Client.active_client.token_connection }
+
+    it 'should return a OAuth2::AccessToken object' do
+      binding.pry
+      expect(token_connection.is_a?(OAuth2::AccessToken))
+    end
+
+    it 'should return include a token' do
+      expect(token_connection.token).to_not be(nil)
+    end
+  end
+end
