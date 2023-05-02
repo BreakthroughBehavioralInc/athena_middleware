5bc4728e2377cefddc131699455a17121955a404
Robert Magomero <rmagomero@mdlive.com>
2023-05-02 12:03:51 -0400

    MLP-2727,MLP-2726,MLP-2729 add create claim, bind insurance to beacon, and check in

--- lib/athena_api/resources/appointments.rb
+++ lib/athena_api/resources/appointments.rb
@@ -9,5 +9,17 @@ module AthenaApi
     def self.patient_appointment_reasons(params)
       execute_request(:get, "#{client.config.base_url}/#{url_version}/#{client.config.practice_id}/patientappointmentreasons", params: params)
     end
+
+    def self.create_claim_for_appointment(appointment_id, params)
+      execute_request(:post, "#{resource_url}/#{appointment_id}/claim", body_params: params)
+    end
+
+    def self.bind_insurance_for_beacon(appointment_id,insurance_id)
+      execute_request(:put, "#{resource_url}/#{appointment_id}/insurances", params: {primaryinsuranceid: insurance_id})
+    end
+
+    def self.check_in_appointment(appointment_id)
+      execute_request(:post, "#{resource_url}/#{appointment_id}/checkin")
+    end
   end
 end
