27a20a9d10cab7623123fab183ec8ea7f882d7c0
Robert Magomero <rmagomero@mdlive.com>
2023-08-17 11:53:18 -0400

    add request to create financial claim

--- lib/athena_api/resources/claims.rb
+++ lib/athena_api/resources/claims.rb
@@ -9,5 +9,9 @@ module AthenaApi
     def self.claim_transactions(claim_id)
       execute_request(:get, "#{resource_url}/#{claim_id}/claimtransactions")
     end
+
+    def self.create_financial_claim(params)
+      execute_request(:post, "#{resource_url}", body_params: params)
+    end
   end
 end
